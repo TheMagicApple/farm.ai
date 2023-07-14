3da43fec290dc43fd842ba5b06427d89246b11fb
DANIEL LIEF <90540132+TheMagicApple@users.noreply.github.com>
2023-07-14 12:55:03 -0700

    setup basic react

---
+++ garden.ai/src/stuff.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Link} from "react-router-dom";
+class Home extends React.Component{
+    render(){
+        return <>
+                GARDEN.AI
+                </>
+    }
+}
+
+
+export {Home}
