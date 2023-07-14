f7f51367a19e3b2bc05541c03bfa86d29604da5f
DANIEL LIEF <90540132+TheMagicApple@users.noreply.github.com>
2023-07-14 16:48:08 -0700

    added initial garden stuff

--- garden.ai/src/App.js
+++ garden.ai/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Home} from "./stuff.jsx";
+import {Home, Garden} from "./stuff.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
           <Route index element={<Home/>}/>
+          <Route path="/garden" element={<Garden/>}/>
       </Routes>
     </BrowserRouter>
 
